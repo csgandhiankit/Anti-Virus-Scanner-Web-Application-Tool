d5119d1e8f15783537ee2c61cab1f4199b510200
Ankit Gandhi <csgandhiankit@gmail.com>
2017-12-06 19:46:09 -0800

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', '127.0.0.1');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'root1234');
-   define('DB_DATABASE', 'Project174');
-   $db = mysql_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD);
-?>
